1b5bb27f811be355aa2603cde330b291d3bc9d74
ssankq <ssankq@gmail.com>
2022-02-25 13:33:37 +0900

    문제 풀이 추가

---
+++ Programmers/Level-1/문자열/수박수박수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let result = '';
+    for(let i = 0; i < n; i++) {
+        i % 2 === 0 ? result += '수' : result +='박'
+    }
+    return result;
+}
+
+let output = solution(5);
+console.log(output);
