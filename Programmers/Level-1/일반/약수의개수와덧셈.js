ffbf23729399ffb7850e03521c0146e0deccd2d6
ssankq <ssankq@gmail.com>
2022-04-27 10:17:53 +0900

    문제풀이 추가

--- Programmers/Level-1/일반/약수의개수와덧셈.js
+++ Programmers/Level-1/일반/약수의개수와덧셈.js
@@ -1,17 +1,29 @@
 // left와 right 사이의 모든 수 중에 약수의 개수가 짝수인 수는 더하고, 홀수인 수는 뺀 수 return
-function solution(left, right) {
-    const numOfDiv = (num, arr = []) => {
-        for(let i = 1; i <= num; i++) {
-            if(num % i === 0) arr.push(i);
-        }
-        return arr.length % 2 === 0; 
-    }
+// function solution(left, right) {
+//     const numOfDiv = (num, arr = []) => {
+//         for(let i = 1; i <= num; i++) {
+//             if(num % i === 0) arr.push(i);
+//         }
+//         return arr.length % 2 === 0; 
+//     }
     
-    let result = 0;
-    for(let i = left; i <= right; i++) {
-        numOfDiv(i) ? result += i : result -= i;
+//     let result = 0;
+//     for(let i = left; i <= right; i++) {
+//         numOfDiv(i) ? result += i : result -= i;
+//     }
+//     return result;
+// }
+
+function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
     }
-    return result;
+    return answer;
 }
 
 let left = 13, right = 17;
