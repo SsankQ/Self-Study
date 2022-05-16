5b0f6b22c44d6a018a8fd7cf3f013fceaff6e64b
ssankq <ssankq@gmail.com>
2022-05-16 20:18:59 +0900

    Front-end Setup

--- NomadCoder/zoomCloneCoding/src/server.js
+++ NomadCoder/zoomCloneCoding/src/server.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
 app.listen(PORT, () => {
-    console.log(`HTTP server listening on ${PORT} Port`);
+    console.log(`HTTP server listening on http://localhost:${PORT}`);
 });
