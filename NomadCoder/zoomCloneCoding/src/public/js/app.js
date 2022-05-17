0e5305e0eee3d6622cf1546ae86404e343c95b47
ssankq <ssankq@gmail.com>
2022-05-17 18:09:18 +0900

    #1.0 ~ #1.9

--- NomadCoder/zoomCloneCoding/src/public/js/app.js
+++ NomadCoder/zoomCloneCoding/src/public/js/app.js
@@ -1,1 +1,39 @@
-alert("Hi!");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from server ❌");
+});
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_msg", input.value));
+  input.value = "";
+};
+
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- NomadCoder/zoomCloneCoding/src/server.js
+++ NomadCoder/zoomCloneCoding/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -9,6 +11,31 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(PORT, () => {
-    console.log(`HTTP server listening on http://localhost:${PORT}`);
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
+
+const server = http.createServer(app); // http 서버
+const wss = new WebSocket.Server({ server }); // ws 서버
+const sockets = []; // each browser
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    msg = msg.toString("utf8");
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_msg":
+        sockets.forEach((aScoket) => {
+          aScoket.send(`${socket.nickname}: ${message.payload}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
+  });
 });
+
+server.listen(PORT, handleListen);
