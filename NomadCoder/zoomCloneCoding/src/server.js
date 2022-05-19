94b7d152a6b41bdfc113e549c8e6d04cc9e98c49
ssankq <ssankq@gmail.com>
2022-05-19 17:05:10 +0900

    [Zoom] #2.4

--- NomadCoder/zoomCloneCoding/src/server.js
+++ NomadCoder/zoomCloneCoding/src/server.js
@@ -20,11 +20,12 @@ const httpServer = http.createServer(app); // http 서버
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 5000);
+  socket.onAny((e) => {
+    console.log(`Socket Event: ${e}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
   });
 });
 
