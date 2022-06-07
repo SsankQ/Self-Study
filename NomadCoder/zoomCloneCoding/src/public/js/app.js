0447d253aace141ac6c37ae1e4e070565bb2529f
ssankq <ssankq@gmail.com>
2022-06-07 17:22:42 +0900

    #3.1

--- NomadCoder/zoomCloneCoding/src/public/js/app.js
+++ NomadCoder/zoomCloneCoding/src/public/js/app.js
@@ -3,10 +3,28 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getCameras();
 
 const getMedia = async () => {
   try {
@@ -23,6 +41,9 @@ const getMedia = async () => {
 getMedia();
 
 const handleMuteClick = () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -32,6 +53,9 @@ const handleMuteClick = () => {
   }
 };
 const handleCameraClick = () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
